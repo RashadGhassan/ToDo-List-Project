ae4054b409854039dfa1dea4cc0018acb6e464df
Rashad Ghassan <rashad.ghassan06@gmail.com>
2023-11-29 22:33:15 +0300

    account info done with half of the functionality

--- lib/screens/account_info_screen.dart
+++ lib/screens/account_info_screen.dart
@@ -51,24 +51,207 @@ class _AccountInfoFormState extends State<AccountInfoForm> {
     });
   }
 
+  bool enable = false;
+  Color bgColor = Colors.white;
+  Color txtColor = Color(0xff187585);
+  String txt = "Edit";
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
+        child: ListView(
           children: [
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Field Required!";
-                }
+            Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0, left: 12),
+                      child: Text(
+                        "Email",
+                        style: TextStyle(
+                          color: Color(0xff666666),
+                          fontSize: 16,
+                          // fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      initialValue: "maamoun.ameenex@gmail.com",
+                      decoration: InputDecoration(
+                        enabled: false,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0, left: 12),
+                      child: Text(
+                        "First Name",
+                        style: TextStyle(
+                          color: Color(0xff666666),
+                          fontSize: 16,
+                          // fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      initialValue: "Maamoun",
+                      decoration: InputDecoration(
+                        enabled: enable,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0, left: 12),
+                      child: Text(
+                        "Last Name",
+                        style: TextStyle(
+                          color: Color(0xff666666),
+                          fontSize: 16,
+                          // fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      initialValue: "Hajtaher",
+                      decoration: InputDecoration(
+                        enabled: enable,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0, left: 12),
+                      child: Text(
+                        "Phone",
+                        style: TextStyle(
+                          color: Color(0xff666666),
+                          fontSize: 16,
+                          // fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      initialValue: "795705082",
+                      decoration: InputDecoration(
+                        prefixText: "+962",
+                        enabled: enable,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0, left: 12),
+                      child: Text(
+                        "Date of Birth",
+                        style: TextStyle(
+                          color: Color(0xff666666),
+                          fontSize: 16,
+                          // fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        enabled: enable,
+                        suffixIcon: MaterialButton(
+                          child: Icon(
+                            Icons.calendar_month,
+                            color: Colors.grey[600],
+                          ),
+                          onPressed: _showDatePicker,
+                        ),
+                        hintText: _dayTime,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+                backgroundColor: bgColor,
+                side: BorderSide(
+                  color: Color(0xff187585),
+                  width: 2,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  enable = !enable;
+                  if (enable) {
+                    bgColor = Color(0xff187585);
+                    txtColor = Colors.white;
+                    txt = "Save";
+                  } else {
+                    bgColor = Colors.white;
+                    txtColor = Color(0xff187585);
+                    txt = "Edit";
+                    if (_formKey.currentState!.validate()) {
+                      var sb = const SnackBar(
+                        content: Text("Email Changed Successfully"),
+                        backgroundColor: Colors.green,
+                        duration: Duration(seconds: 2),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(sb);
+                    }
+                  }
+                });
               },
-              decoration: InputDecoration(
-                prefixText: "Mamon",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  txt,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: txtColor,
+                  ),
                 ),
               ),
             ),
