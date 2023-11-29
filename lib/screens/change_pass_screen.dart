e23bd5a107d98596d305a412bc81def516b1a204
Rashad Ghassan <rashad.ghassan06@gmail.com>
2023-11-29 20:42:05 +0300

    added two new dart files to be done

---
+++ lib/screens/change_pass_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChangePass extends StatefulWidget {
+  const ChangePass({super.key});
+
+  @override
+  State<ChangePass> createState() => _ChangePassState();
+}
+
+class _ChangePassState extends State<ChangePass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
