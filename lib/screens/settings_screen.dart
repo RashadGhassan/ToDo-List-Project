c9781c412f493fffe193c99ad4e84cb96c85c6a9
Rashad Ghassan <rashad.ghassan06@gmail.com>
2023-11-29 20:42:34 +0300

    settings screen done

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/widgets/drawer.dart';
+import 'package:to_do_list/widgets/list_tile_settings.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      drawer: DrawerWidget(),
+      body: Column(
+        children: [
+          SizedBox(height: 8),
+          ListTileSetting(
+            text: "Account Info",
+            routeName: "/accountInfoPage",
+          ),
+          SizedBox(height: 8),
+          ListTileSetting(
+            text: "Change Email",
+            routeName: "/changeEmailPage",
+          ),
+          SizedBox(height: 8),
+          ListTileSetting(
+            text: "Change Password",
+            routeName: "/changePassPage",
+          ),
+        ],
+      ),
+    );
+  }
+}
